e08e64e42b9aefb3580f6b621b40df55d878a555
mikfra <36963959+mikfra@users.noreply.github.com>
2019-09-26 16:38:45 -0400

    Added other stuff

--- ELK_Builders.sh
+++ ELK_Builders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Check to see if singularity is installed 
+#Check to see if singularity is installed
 #command -v singularity >/dev/null 2>&1 || {echo >&2 "Singularity Not installed"; exit 1; }
 
 declare -a ELK=("elasticsearch" "logstash" "kibana")
@@ -8,8 +8,8 @@ declare -a FUNCT=("make_elasticsearch" "make_logstash" "make_kibana")
 
 
 #Make root RunDir and root directories for singularity containers
-read -p "Enter the Directory you would like to install Elasticsearch[Default=  /containers]: " read_dir 
-read_dir=${read_dir:-"/hello"}
+read -p "Enter the Directory you would like to install Elasticsearch[Default=  /containers]: " read_dir
+read_dir=${read_dir:-"/scratch/x_scratch/stack"}
 echo "[Directory= $read_dir]"
 read -p "What user will be running the containers. i.e. not root?[Default= nobody]"
 user=${user:-"user"}
@@ -25,14 +25,12 @@ singularity exec \
 }
 make_elasticsearch() {
 echo "
-#!/bin/bash 
-
+#!/bin/bash
 pwd=$(pwd)
 rundir=$pwd/rundir
 contdir=/usr/share/elasticsearch
-
 singularity instance start \\
-	-B $rundir/config:$contdir/config \\ 
+	-B $rundir/config:$contdir/config \\
 	-B $rundir/data:$contdir/data \\
 	-B $rundir/logs:$contdir/logs \\
 	$pwd/es_7.2.0.sif elastic" > $read_dir/$x/run.sh
@@ -50,9 +48,9 @@ done
 }
 
 config_yml_elasticsearch() {
-if [ -f "/hello/elasticsearch/rundir/config/elasticsearch.yml"]; then
+if [ -f "$read_dir/elasticsearch/rundir/config/elasticsearch.yml" ]; then
 	echo [Elasticsearch.yml exists]
-	echo"
+	echo "
 	cluster.name: "${HOSTNAME}_cluster"
 	network.host: 0.0.0.0
 	node.master: true
@@ -61,10 +59,9 @@ if [ -f "/hello/elasticsearch/rundir/config/elasticsearch.yml"]; then
 	    - 0.0.0.0
 	discovery.zen.minimum_master_nodes: 1
 	discovery.zen.ping.unicast.hosts: ["0.0.0.0:9300"]
-	path: 
+	path:
 	    logs: /usr/share/elasticsearch/logs/${HOSTNAME}
-	    data: /usr/share/elasticsearch/data/${HOSTNAME}
-	" > $read_dir/$x/rundir/config/$x.yml
+	    data: /usr/share/elasticsearch/data/${HOSTNAME}" > $read_dir/$x/rundir/config/$x.yml
 else
 	echo "[No file exist? What You Doin'! Forget about it!]"
 fi
@@ -72,14 +69,17 @@ fi
 
 make_logstash() {
 echo "
-#!/bin/bash 
-
+#!/bin/bash
 singularity shell \\
-	-B ./rundir:/usr/share/logstash \\
-	-B /home/mrf/pcaps/csv:/pcaps \\
+	-B ./rundir/config:/usr/share/logstash/config \\
+	-B ./rundir/modules:/usr/share/logstash/modules \\
+	-B ./rundir/data:/usr/share/logstash/data \\
+	-B ./rundir/pipeline:/usr/share/logstash/pipeline \\
+	-B ./rundir/tools:/usr/share/logstash/tools \\
+	-B ./rundir/x-pack:/usr/share/logstash/x-pack \\
 	./logstash_7.3.2.sif" > $read_dir/$x/run.sh
 
-lcpdir=(config data modules piplines tools x-pack)
+lcpdir=(config data modules pipeline tools x-pack)
 cdir=/usr/share/logstash
 for i in "${lcpdir[@]}"; do
 	if [ -d "$read_dir/$x/rundir/$i" ]; then
@@ -91,14 +91,23 @@ for i in "${lcpdir[@]}"; do
 done
 }
 
+config_yml_logstash() {
+if [ -f "$read_dir/$x/rundir/config/$x.yml" ]; then
+    echo "
+		http.host: "0.0.0.0"
+    xpack.monitoring.elasticsearch.hosts: [ "http://elasticsearch:9200" ]
+    " > $read_dir/$x/rundir/config/$x.yml
+else
+	echo "[No file exist? What You Doin'! Forget about it!]"
+fi
+}
+
 make_kibana() {
 echo "
-#!/bin/bash 
-
+#!/bin/bash
 pwd=$(pwd)
 rundir=$pwd/rundir
 contdir=/usr/share/kibana
-
 singularity instance start \\
 	-B $rundir/config:$contdir/config \\
 	-B $rundir/data:$contdir/data \\
@@ -118,20 +127,30 @@ for i in "${kcpdir[@]}"; do
 done
 }
 
-#Checking/Creating associated containers direcotry 
+config_yml_kibana() {
+if [ -f "$read_dir/$x/rundir/config/$x.yml" ]; then
+    echo "
+		server.name: kibana
+		server.host: "0"
+		elasticsearch.hosts: [ "http://0:9200" ]
+    " > $read_dir/$x/rundir/config/$x.yml
+else
+	echo "[No file exist? What You Doin'! Forget about it!]"
+fi
+}
+
+#Checking/Creating associated containers direcotry
 echo "[Setting up containers directory and runscripts]"
 
 for x in ${ELK[*]}; do
 	mkdir -p $read_dir/$x/rundir
 	touch $read_dir/$x/run.sh
-	if [ -f "$read_dir/$x/$x.sif" ]; then 
+	if [ -f "$read_dir/$x/${x}_7.3.2.sif" ]; then
 		echo "[Containers are already installed]"
 	else
 		echo "[Pulling $x from DockerHub]"
-		singularity pull $read_dir/$x/$x.sif docker://$x:7.3.2
+		singularity pull $read_dir/$x/${x}_7.3.2.sif docker://$x:7.3.2
 	fi
 	make_$x
 	config_yml_$x
 done
-
-
